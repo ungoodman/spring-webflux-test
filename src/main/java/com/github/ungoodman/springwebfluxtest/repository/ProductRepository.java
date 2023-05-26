a9dfeb1f8e87757cfc6f9dd8913c267f040e6e92
ungoodman <suchanej.19@gmail.com>
2023-05-26 19:01:51 +0700

    ProductRepository: add findByStatusCode(), findByIdAndStatusCode

--- src/main/java/com/github/ungoodman/springwebfluxtest/repository/ProductRepository.java
+++ src/main/java/com/github/ungoodman/springwebfluxtest/repository/ProductRepository.java
@@ -3,11 +3,15 @@ package com.github.ungoodman.springwebfluxtest.repository;
 import com.github.ungoodman.springwebfluxtest.model.entity.Product;
 import org.springframework.data.r2dbc.repository.R2dbcRepository;
 import org.springframework.stereotype.Repository;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 import java.io.Serializable;
+import java.util.UUID;
 
 @Repository
 public interface ProductRepository extends R2dbcRepository<Product, Serializable> {
+    Flux<Product> findByStatusCode(String statusCode);
+    Mono<Product> findByIdAndStatusCode(UUID id, String statusCode);
     Mono<Product> findByProductCode(String code);
 }
